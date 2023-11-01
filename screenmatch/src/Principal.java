d53525bf6026f6145c3c4e683c1c507a2014ed29
Chai Q <101228665+chaiaq@users.noreply.github.com>
2023-10-31 21:41:40 -0300

    31/10

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,12 +1,13 @@
+import br.com.alura.screematch.modelos.Filme;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		Filme meuFilme = new Filme();
 		
-		meuFilme.nome = "O Poderoso Chefão";
-		meuFilme.anoDeLancamento = 1970;
-		meuFilme.duracaoEmMinutos = 180;
+		meuFilme.setNome ("O Poderoso Chefão");
+		meuFilme.setAnoDeLancamento(1970);
+		meuFilme.setDuracaoEmMinutos(180);
 		
 		/*
 		System.out.println(meuFilme);
@@ -21,10 +22,11 @@ public class Principal {
 		meuFilme.avalia(5);
 		meuFilme.avalia(10);
 		
-		System.out.println(meuFilme.somaDasAvaliacoes);
-		System.out.println(meuFilme.totalDeAvaliacoes);
+		// System.out.println(meuFilme.somaDasAvaliacoes);
+		// System.out.println(meuFilme.totalDeAvaliacoes);
 		
 		System.out.println(meuFilme.pegaMedia());
+		System.out.println(meuFilme.getTotalDeAvaliacoes());
 		
 	}
 
